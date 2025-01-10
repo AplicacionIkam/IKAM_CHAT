d704cf26b42258b0a0fadac85a950e29ee6a302e
Zaywop06 <160089073+Zaywop06@users.noreply.github.com>
2025-01-10 11:44:38 -0600

    Correccion en archivo chat

--- app/(tabs)/menu/chat.tsx
+++ app/(tabs)/menu/chat.tsx
@@ -75,11 +75,9 @@ const Chat = () => {
   useEffect(() => {
     const unsubscribe = suscribirseAPymes((pymesData) => {
       setPymes(
-<<<<<<< HEAD
-        pymesData.sort((a, b) => (a.nombre_pyme || "").localeCompare(b.nombre_pyme || ""))
-=======
-        pymesData.sort((a, b) => (a.nombre_pyme || "").localeCompare(b.nombre_pyme))
->>>>>>> cab09c2ba8368dd21287acf500877222f06bb4a0
+        pymesData.sort((a, b) =>
+          (a.nombre_pyme || "").localeCompare(b.nombre_pyme || "")
+        )
       );
     });
     return () => unsubscribe && unsubscribe();
